da370f5481e529953b167ec68b5d4e997f8fe07c
VictorPko73 <152915385+VictorPko73@users.noreply.github.com>
2025-07-20 17:47:28 +0000

    funcionando

--- src/app.js
+++ src/app.js
@@ -5,7 +5,22 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function () {
   //write your code here
+  
+  let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+  let action = ['ate', 'peed', 'crushed', 'broke'];
+  let what = ['my homework', 'my phone', 'the car'];
+  let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+  let randomWho = who[Math.floor(Math.random()* who.length)]
+  let randomAction = action[Math.floor(Math.random()* action.length)]
+  let randomWhat = what[Math.floor(Math.random()* what.length)]
+  let randomWhen = when[Math.floor(Math.random()* when.length)]
+  
+  let excuse = `${randomWho}, ${randomAction}, ${randomWhat}, ${randomWhen}`
   console.log("Hello Rigo from the console!");
+  document.getElementById('excuse').innerHTML = excuse;
+  
+
 };
